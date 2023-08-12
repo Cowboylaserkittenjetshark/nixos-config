dd796393788e3066b8befe38565961ddc34c841b
Cowboylaserkittenjetshark <82691052+Cowboylaserkittenjetshark@users.noreply.github.com>
2023-08-12 19:27:15 -0400

    Plugins for system zsh config

--- modules/zsh.nix
+++ modules/zsh.nix
@@ -1,5 +1,14 @@
 { pkgs, ... }: {
-  programs.zsh.enable = true;
+  programs.zsh = {
+    enable = true;
+    autosuggestions.enable = true;
+    syntaxHighlighting = {
+      enable = true;
+      patterns = { "rm -rf *" = "fg=black,bg=red"; };
+      styles = { "alias" = "fg=magenta"; };
+      highlighters = [ "main" "brackets" "pattern" ];
+    };
+  };
   environment.shells = with pkgs; [ zsh ];
   # enable zsh autocompletion for system packages (systemd, etc)
   environment.pathsToLink = [ "/share/zsh" ];
