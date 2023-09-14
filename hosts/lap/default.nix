c6efe419edf848af04cb0e10fba8011f26b6900d
Cowboylaserkittenjetshark <82691052+Cowboylaserkittenjetshark@users.noreply.github.com>
2023-09-14 09:17:10 -0400

    Enable pam_u2f

--- hosts/lap/default.nix
+++ hosts/lap/default.nix
@@ -9,6 +9,7 @@
     ./hardware-configuration.nix
     ../../modules/core.nix
     ../../modules/sddm.nix
+    ../../modules/pam_u2f.nix
     # I currently am not satisified with plymouth in Nix
     # https://github.com/NixOS/nixpkgs/issues/26722
     # ../../modules/plymouth.nix
