c35aaf95a3573d3d0683841fbfba5aa27e38821c
Cowboylaserkittenjetshark <82691052+Cowboylaserkittenjetshark@users.noreply.github.com>
2023-09-18 16:39:25 -0400

    u2f config: add cue, sddm , swaylock

--- modules/pam_u2f.nix
+++ modules/pam_u2f.nix
@@ -1,6 +1,11 @@
 {...}: {
-  security.pam.services = {
-    login.u2fAuth = true;
-    sudo.u2fAuth = true;
+  security.pam = {
+    u2f.cue = true;
+    services = {
+      login.u2fAuth = true;
+      swaylock.u2fAuth = true;
+      sddm.u2fAuth = true;
+      sudo.u2fAuth = true;
+    };
   };
 }
