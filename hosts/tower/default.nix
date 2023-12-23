6a3771b779f739eb4cd7557e005615ca46e079d6
Cowboylaserkittenjetshark <82691052+Cowboylaserkittenjetshark@users.noreply.github.com>
2023-12-23 18:35:06 -0500

    Add host config for the tower

---
+++ hosts/tower/default.nix
@@ -0,0 +1,67 @@
+{
+  inputs,
+  config,
+  pkgs,
+  ...
+}: {
+  imports = [
+    # Include the results of the hardware scan.
+    ./hardware-configuration.nix
+    ../../modules/core.nix
+    ../../modules/sddm.nix
+    ../../modules/pam_u2f.nix
+    ../../modules/printing.nix
+    ../../modules/steam.nix
+    # I currently am not satisified with plymouth in Nix
+    # https://github.com/NixOS/nixpkgs/issues/26722
+    # ../../modules/plymouth.nix
+  ];
+
+  boot = {
+    # Enable systemd in phase 1. Used for unlocking root partition with FIDO2/TPM
+    initrd.systemd.enable = true;
+    # Use the systemd-boot EFI boot loader.
+    loader = {
+      systemd-boot.enable = true;
+      efi.canTouchEfiVariables = true;
+    };
+  };
+
+  networking = {
+    hostName = "tower"; # Define your hostname.
+    networkmanager.enable = true; # Easiest to use and most distros use this by default.
+  };
+
+  # Set your time zone.
+  time.timeZone = "America/New_York";
+
+  services.pipewire = {
+    enable = true;
+    alsa = {
+      enable = true;
+      support32Bit = true;
+    };
+    pulse.enable = true;
+  };
+  
+  environment.systemPackages = with pkgs; [
+    vim
+    git
+    wget
+    inputs.helix.packages."${pkgs.system}".helix
+  ];
+
+  programs.hyprland = {
+    enable = true;
+    package = inputs.hyprland.packages.${pkgs.system}.hyprland;
+  };
+  environment.sessionVariables.NIXOS_OZONE_WL = "1";
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It's perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "23.11"; # Did you read the comment?
+}
