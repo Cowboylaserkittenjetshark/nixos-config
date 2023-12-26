42eb9a887a683c0de6af20676b4230e8f1cb0f84
Cowboylaserkittenjetshark <cowboylaserkittenjetshark@cblkjs.com>
2023-12-26 16:51:20 -0500

    Full initial tower config

--- flake.nix
+++ flake.nix
@@ -49,6 +49,14 @@
           ./users/cblkjs
         ];
       };
+      tower = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        specialArgs = {inherit inputs;};
+        modules = [
+          ./hosts/tower
+          ./users/cblkjs
+        ];
+      };
     };
   };
 
