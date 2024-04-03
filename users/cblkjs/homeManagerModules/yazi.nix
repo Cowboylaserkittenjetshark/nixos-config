64e93c5e7c1efeccda671c65ec92c5c3bff24dab
Cowboylaserkittenjetshark <82691052+Cowboylaserkittenjetshark@users.noreply.github.com>
2024-04-03 18:06:39 -0400

    Add aliases for bat and yazi

--- users/cblkjs/homeManagerModules/yazi.nix
+++ users/cblkjs/homeManagerModules/yazi.nix
@@ -3,6 +3,8 @@
     enable = true;
   };
 
+  shellAliases = {y = "yazi";};
+
   xdg.configFile = {
     "yazi/theme.toml".source = "${inputs.catppuccin-yazi}/themes/mocha.toml";
     "yazi/Catppuccin-mocha.tmTheme".source = "${inputs.catppuccin-bat}/themes/Catppuccin Mocha.tmTheme";
