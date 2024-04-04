ca36ccf28513b7c25f4f7278fb19fca04f94fed2
Cowboylaserkittenjetshark <82691052+Cowboylaserkittenjetshark@users.noreply.github.com>
2024-04-03 21:05:11 -0400

    Yazi: Settings

--- users/cblkjs/homeManagerModules/yazi.nix
+++ users/cblkjs/homeManagerModules/yazi.nix
@@ -1,6 +1,13 @@
 {inputs, ...}: {
   programs.yazi = {
     enable = true;
+    settings = {
+      manager = {
+        show_hidden = true;
+        show_symlink = true;
+        scrolloff = 200; # Force centering the cursor
+      };
+    };
   };
 
   home.shellAliases = {y = "yazi";};
