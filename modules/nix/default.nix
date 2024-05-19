01943e92b9a11f39e4db6ded1219fb5614ad93f0
Cowboylaserkittenjetshark <82691052+Cowboylaserkittenjetshark@users.noreply.github.com>
2024-05-18 22:37:50 -0400

    Nix registry: pin nixpkgs to current system revision

--- modules/nix/default.nix
+++ modules/nix/default.nix
@@ -1,6 +1,10 @@
-{...}: {
+{inputs, ...}: {
   imports = [
     ./builders.nix
     ./substituters.nix
   ];
+
+  # Pin registry entry for nixpkgs to the current generation's version
+  # Prevents downloading nixpkgs pretty much every time nix shell/run is run
+  nix.registry.nixpkgs.flake = inputs.nixpkgs;
 }
