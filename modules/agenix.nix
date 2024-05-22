9662b698d8c2c86c4d04f3a34cb35c925273c838
Cowboylaserkittenjetshark <82691052+Cowboylaserkittenjetshark@users.noreply.github.com>
2024-05-21 23:39:07 -0400

    VPNs: Don't route tailscale traffic; Use stock .ovpn file from Windscribe and place modifications directly in vpns.nix

--- modules/agenix.nix
+++ modules/agenix.nix
@@ -4,6 +4,7 @@
   ...
 }: {
   imports = [inputs.agenix.nixosModules.default];
+  age.identityPaths = ["/etc/ssh/ssh_host_ed25519_key"];
 
   environment.systemPackages = [
     inputs.agenix.packages.${pkgs.system}.default
--- modules/network/vpns.nix
+++ modules/network/vpns.nix
@@ -1,6 +1,11 @@
 {config, ...}: {
   services.openvpn.servers = {
-    windscribe-Atlanta-Mountain.config = '' config ${config.age.secrets.Windscribe-Atlanta-Mountain-conf.path} '';
+    windscribe-Atlanta-Mountain.config = ''
+      config ${config.age.secrets.Windscribe-Atlanta-Mountain-conf.path}
+      
+      auth-user-pass ${config.age.secrets.Windscribe-Atlanta-Mountain-auth.path}
+      route 10.64.0.0 255.192.0.0 net_gateway
+    '';
   };
   age.secrets = {
     Windscribe-Atlanta-Mountain-conf = {
