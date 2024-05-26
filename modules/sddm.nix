6a6978d7060be45c311b54505ec64c5e9f0c63d9
Cowboylaserkittenjetshark <82691052+Cowboylaserkittenjetshark@users.noreply.github.com>
2024-05-26 16:32:18 -0400

    SDDM: Use upstream theme

--- modules/sddm.nix
+++ modules/sddm.nix
@@ -3,12 +3,26 @@
   pkgs,
   ...
 }: {
-  environment.systemPackages = with pkgs; [
-    (pkgs.callPackage ../pkgs/catppuccin-sddm.nix {})
+  environment.systemPackages = [
+    (pkgs.catppuccin-sddm.override {
+      flavor = "mocha";
+      font = "Noto Sans";
+      fontSize = "9";
+      background = "${config.desktopAssets.wallpaper}";
+      loginBackground = true;
+    })
+    (pkgs.where-is-my-sddm-theme.override {
+      themeConfig.General = {
+      background = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
+      backgroundMode = "none";
+      };
+    })
   ];
   services.displayManager.sddm = {
     enable = true;
     wayland.enable = true;
     theme = "catppuccin-mocha";
+    package = pkgs.kdePackages.sddm;
+    extraPackages = with pkgs; [qt6.qt5compat qt6.qtsvg];
   };
 }
