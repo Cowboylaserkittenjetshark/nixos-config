357645322b1bad375bebd99652b8ee773c10f256
Cowboylaserkittenjetshark <82691052+Cowboylaserkittenjetshark@users.noreply.github.com>
2024-06-01 16:13:25 -0400

    Use latest kernel

--- modules/core.nix
+++ modules/core.nix
@@ -13,6 +13,8 @@
   users.mutableUsers = false;
   security.rtkit.enable = true;
   boot = {
+    # Use latest kernel
+    kernelPackages = pkgs.linuxPackages_latest;
     # Enable systemd in phase 1. Used for unlocking root partition with FIDO2/TPM
     initrd.systemd.enable = true;
     # Use the systemd-boot EFI boot loader.
