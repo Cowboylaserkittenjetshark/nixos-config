7765f07e194f401d97308e817233b4520e630c30
Cowboylaserkittenjetshark <82691052+Cowboylaserkittenjetshark@users.noreply.github.com>
2024-06-07 12:50:30 -0400

    Chromium: Add Stylus extension

--- users/cblkjs/homeManagerModules/programs/chromium.nix
+++ users/cblkjs/homeManagerModules/programs/chromium.nix
@@ -8,6 +8,8 @@
       {id = "nngceckbapebfimnlniiiahkandclblb";}
       # Dark Reader
       {id = "eimadpbcbfnmbkopoojfekhnkhdbieeh";}
+      # Stylus
+      {id = "clngdbkpkpeebahjckkjfobafhncgmne";}
       # Catppuccin Mocha Theme
       {id = "bkkmolkhemgaeaeggcmfbghljjjoofoh";}
     ];
