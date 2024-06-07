9f8d5b97d6c0e4e85c6f3f4ee7149aac14f827e8
Cowboylaserkittenjetshark <82691052+Cowboylaserkittenjetshark@users.noreply.github.com>
2024-06-07 13:20:04 -0400

    Catppuccin: Use latest rev until upstream module updates

--- users/cblkjs/homeManagerModules/catppuccin.nix
+++ users/cblkjs/homeManagerModules/catppuccin.nix
@@ -7,5 +7,9 @@
     enable = true;
     flavor = "mocha";
     accent = "green";
+    sources.foot = builtins.fetchGit {
+      url = "https://github.com/catppuccin/foot";
+      rev = "80756a4d63ea4fae4d0fdd793017370f8b8b12ac";
+    };
   };
 }
