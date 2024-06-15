b370f366723f3d7fc5cfe6810e2d10481621654d
Cowboylaserkittenjetshark <82691052+Cowboylaserkittenjetshark@users.noreply.github.com>
2024-06-15 14:39:03 -0400

    Revert "Catppuccin: Use latest rev until upstream module updates"
    This reverts commit 9f8d5b97d6c0e4e85c6f3f4ee7149aac14f827e8.

--- users/cblkjs/homeManagerModules/catppuccin.nix
+++ users/cblkjs/homeManagerModules/catppuccin.nix
@@ -7,9 +7,5 @@
     enable = true;
     flavor = "mocha";
     accent = "green";
-    sources.foot = builtins.fetchGit {
-      url = "https://github.com/catppuccin/foot";
-      rev = "80756a4d63ea4fae4d0fdd793017370f8b8b12ac";
-    };
   };
 }
