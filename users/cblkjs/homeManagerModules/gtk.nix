9ede20a65077c192f56a53a5a9f3b9811731a5fe
Cowboylaserkittenjetshark <82691052+Cowboylaserkittenjetshark@users.noreply.github.com>
2024-06-21 02:25:54 -0400

    GTK theme: Use Catppuccin Colloid tweak

--- users/cblkjs/homeManagerModules/gtk.nix
+++ users/cblkjs/homeManagerModules/gtk.nix
@@ -3,11 +3,16 @@
   config,
   ...
 }: {
-  gtk.enable = true;
-
-  xdg.configFile = {
-    "gtk-4.0/assets".source = "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}/gtk-4.0/assets";
-    "gtk-4.0/gtk.css".source = "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}/gtk-4.0/gtk.css";
-    "gtk-4.0/gtk-dark.css".source = "${config.gtk.theme.package}/share/themes/${config.gtk.theme.name}/gtk-4.0/gtk-dark.css";
+  gtk = {
+    enable = true;
+    theme = {
+      name = "Colloid-Green-Dark-Compact-Catppuccin";
+      package = pkgs.colloid-gtk-theme.override {
+        themeVariants = [ "green" ];
+        colorVariants = [ "dark" ];
+        sizeVariants = [ "compact" ];
+        tweaks = [ "catppuccin" "black" "rimless" ];
+      };
+    };
   };
 }
