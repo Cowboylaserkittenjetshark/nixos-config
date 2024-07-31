7f6f931c362c1165adea78b8849a1b48e0d9f54b
Cowboylaserkittenjetshark <82691052+Cowboylaserkittenjetshark@users.noreply.github.com>
2024-07-31 01:40:09 -0400

    feat(homelab/adguardhome): force disable systemd-resolved and elevate privileges on install script Dynamic user is incompatible with how agenix permissions are set

--- modules/homelab/adguardhome.nix
+++ modules/homelab/adguardhome.nix
@@ -5,41 +5,43 @@
   ...
 }:
 with lib; {
-  services.adguardhome = {
-    enable = true;
-    mutableSettings = false;
-    settings = {
-      users = [
-        {
-          name = "admin";
-          password = "@adguard-home-webui-password@";
-        }
-      ];
-      http.address = "0.0.0.0:3000";
-      language = "en";
-      dns = {
-        bind_hosts = ["0.0.0.0"];
-        port = 53;
-        bootstrap_dns = [
-          "9.9.9.10"
-          "149.112.112.10"
-          "2620:fe::10"
-          "2620:fe::fe:10"
+  services = {
+    adguardhome = {
+      enable = true;
+      mutableSettings = false;
+      settings = {
+        users = [
+          {
+            name = "admin";
+            password = "@adguard-home-webui-password@";
+          }
+        ];
+        http.address = "0.0.0.0:3000";
+        language = "en";
+        dns = {
+          bind_hosts = ["0.0.0.0"];
+          port = 53;
+          bootstrap_dns = [
+            "9.9.9.10"
+            "149.112.112.10"
+            "2620:fe::10"
+            "2620:fe::fe:10"
+          ];
+        };
+        filtering.rewrites = mkIf config.homelab.vpnAccess.enable [
+          {
+            domain = "*.${config.homelab.domain}";
+            answer = "${config.homelab.vpnAccess.address}";
+          }
+          {
+            domain = "${config.homelab.domain}";
+            answer = "${config.homelab.vpnAccess.address}";
+          }
         ];
       };
-      filtering.rewrites = mkIf config.homelab.vpnAccess.enable [
-        {
-          domain = "*.${config.homelab.domain}";
-          answer = "${config.homelab.vpnAccess.address}";
-        }
-        {
-          domain = "${config.homelab.domain}";
-          answer = "${config.homelab.vpnAccess.address}";
-        }
-      ];
     };
+    resolved.enable = mkForce false;
   };
-
   networking.firewall.interfaces = mkIf config.homelab.vpnAccess.enable {
     ${config.homelab.vpnAccess.interface} = {
       allowedTCPPorts = [53];
@@ -55,17 +57,15 @@ with lib; {
   age.secrets.adguard-home-webui-password = {
     file = ../../secrets/adguard-home-webui-password.age;
     mode = "400";
-    owner = "adguardhome";
-    group = "adguardhome";
   };
 
   systemd.services.adguardhome = {
     serviceConfig.ExecStartPre = mkAfter [
-      (pkgs.writeShellScript "install-adguard-home-webui-password" ''
+     "+${(pkgs.writeShellScript "install-adguard-home-webui-password" ''
         secret=$(<${config.age.secrets.adguard-home-webui-password.path})
         configFile=/var/lib/AdGuardHome/AdGuardHome.yaml
         ${pkgs.gnused}/bin/sed -i "s#@adguard-home-webui-password@#$secret#" "$configFile"
-      '')
+      '')}"
     ];
   };
 }
