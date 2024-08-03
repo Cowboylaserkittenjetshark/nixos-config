aea8e87884f353ce46272946662560357b1fbac8
Cowboylaserkittenjetshark <82691052+Cowboylaserkittenjetshark@users.noreply.github.com>
2024-08-03 19:22:32 -0400

    fix(tower): lazy fix for nh flake path inconsistencies across hosts

--- hosts/tower/default.nix
+++ hosts/tower/default.nix
@@ -60,6 +60,11 @@
     };
   };
 
+  programs.nh = {
+    enable = true;
+    flake = "/home/cblkjs/nixos-config";
+  };
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It's perfectly fine and recommended to leave
