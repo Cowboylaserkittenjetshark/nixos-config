2d3e60d895bf4863f38032c1cf40c71ce22e2d57
Cowboylaserkittenjetshark <82691052+Cowboylaserkittenjetshark@users.noreply.github.com>
2024-08-05 00:50:41 -0400

    feat(No2TypeT): remove desktop components

--- hosts/No2TypeT/default.nix
+++ hosts/No2TypeT/default.nix
@@ -7,7 +7,6 @@
 }: {
   imports = [
     ./hardware-configuration.nix
-    ../../modules/desktop.nix
     ../../modules/server.nix
     # Using ancient nvidia gpu :/
     ../../modules/hardware/nvidia.nix
