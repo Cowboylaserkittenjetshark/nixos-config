59dadea0b786f5ddd2f71fb8aeae911cfaadb8b4
Cowboylaserkittenjetshark <82691052+Cowboylaserkittenjetshark@users.noreply.github.com>
2024-08-09 15:08:58 -0400

    fix(pipewire): disable camera monitoring to fix battery-draining bug

--- modules/services/pipewire.nix
+++ modules/services/pipewire.nix
@@ -2,6 +2,11 @@
   config = lib.mkIf (builtins.elem "audio" config.systemAttributes.capabilities) {
     services.pipewire = {
       enable = true;
+      wireplumber.extraConfig."10-disable-camera" = {
+        "wireplumber.profiles" = {
+          main."monitor.libcamera" = "disabled";
+        };
+      };
       alsa = {
         enable = true;
         support32Bit = true;
