90687ed22a048d0033b127c0e6b7b00ca7398bb5
Cowboylaserkittenjetshark <82691052+Cowboylaserkittenjetshark@users.noreply.github.com>
2024-08-10 14:51:56 -0400

    feat(tower): enable trim

--- hosts/tower/default.nix
+++ hosts/tower/default.nix
@@ -74,6 +74,8 @@
 
   gaming.enable = true;
 
+  services.fstrim.enable = true;
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It's perfectly fine and recommended to leave
