d2f2dae74002a2e6551644d7d3527e9d98e14adc
Cowboylaserkittenjetshark <82691052+Cowboylaserkittenjetshark@users.noreply.github.com>
2024-09-26 19:55:46 -0400

    feat(homelab/adguardhome): define categories of upstream servers

--- modules/homelab/adguardhome.nix
+++ modules/homelab/adguardhome.nix
@@ -5,6 +5,30 @@
   ...
 }: let
   inherit (lib) mkIf mkForce mkAfter;
+  providers = {
+    Quad9 = {
+      DoH = ["https://dns.quad9.net/dns-query"];
+      IPv4 = [
+        "9.9.9.9"
+        "149.112.112.112"
+      ];
+    };
+    ControlD = {
+      DoH = ["https://freedns.controld.com/p0"];
+      IPv4 = [
+        "76.76.2.0"
+        "76.76.10.0"
+      ];
+    };
+    Cloudflare = rec {
+      DoH = ["https://dns.cloudflare.com/dns-query"];
+      IPv4 = [
+        "1.1.1.1"
+        "1.0.0.1"
+      ];
+      all = DoH ++ IPv4;
+    };
+  };
 in {
   config = mkIf config.homelab.enable {
     services = {
@@ -20,15 +44,12 @@ in {
           ];
           http.address = "0.0.0.0:3000";
           language = "en";
-          dns = {
+          dns = with providers; {
             bind_hosts = ["0.0.0.0"];
             port = 53;
-            bootstrap_dns = [
-              "9.9.9.10"
-              "149.112.112.10"
-              "2620:fe::10"
-              "2620:fe::fe:10"
-            ];
+            bootstrap_dns = Quad9.IPv4 ++ ControlD.IPv4;
+            upstream_dns = Quad9.DoH ++ ControlD.DoH;
+            fallback_dns = Quad9.IPv4 ++ ControlD.IPv4 ++ Cloudflare.all;
           };
           filtering.rewrites = mkIf config.homelab.vpnAccess.enable [
             {
