ef2b8459a62853a7c224e892c8b4aaa9f34b6db4
Cowboylaserkittenjetshark <82691052+Cowboylaserkittenjetshark@users.noreply.github.com>
2024-09-27 20:49:20 -0400

    feat(Hyprland): overhaul config Changes based on: - https://github.com/end-4/dots-hyprland - https://github.com/mylinuxforwork/dotfiles - https://github.com/prasanthrangan/hyprdots

--- users/cblkjs/homeManagerModules/hypr/hyprland.nix
+++ users/cblkjs/homeManagerModules/hypr/hyprland.nix
@@ -17,20 +17,26 @@
       general {
         gaps_in = 5
         gaps_out = 5
-        border_size = 2
+        gaps_workspaces = 50
+        border_size = 1
         col.active_border = $pink $lavender
         col.inactive_border = $surface0
       }
 
       decoration {
-        rounding = 7
+        rounding = 16
 
         blur {
           enabled = true
-          size = 3
-          passes = 1
+          xray = true
+          special = false
+          size = 1
+          passes = 4
+          popups = true
+          popups_ignorealpha = 0.6
         }
       }
+
 
       input {
         touchpad {
@@ -40,16 +46,22 @@
       }
 
       animations {
-        bezier=overshot,0.7,0.6,0.1,1.1
-        bezier=linear,0,0,1,1
-        bezier=bounce,1,1.6,0.1,0.85
+        bezier = md3_decel, 0.05, 0.7, 0.1, 1
+        bezier = md3_accel, 0.3, 0, 0.8, 0.15
+        bezier = menu_decel, 0.1, 1, 0, 1
+        bezier = menu_accel, 0.38, 0.04, 1, 0.07
 
-        animation=windows,1,5,overshot,popin 80%
-        animation=fade,1,3,overshot
-        animation=workspaces,1,6,overshot,slide
-        animation=border,1,2,overshot
-        animation=borderangle,1,40,default,loop
-        animation=layers,1,5,overshot,popin 80%
+        animation = windows, 1, 3, md3_decel, popin 60%
+        animation = windowsIn, 1, 3, md3_decel, popin 60%
+        animation = windowsOut, 1, 3, md3_accel, popin 60%
+        animation = border, 1, 10, default
+        animation = fade, 1, 3, md3_decel
+        animation = layersIn, 1, 3, menu_decel, slide
+        animation = layersOut, 1, 1.6, menu_accel
+        animation = fadeLayersIn, 1, 2, menu_decel
+        animation = fadeLayersOut, 1, 4.5, menu_accel
+        animation = workspaces, 1, 7, menu_decel, slide
+        animation = specialWorkspace, 1, 3, md3_decel, slidevert
       }
 
       dwindle {
@@ -59,6 +71,9 @@
 
       misc {
         disable_hyprland_logo	= true
+        vrr = 1
+        new_window_takes_over_fullscreen = 2
+        initial_workspace_tracking = 2
         enable_swallow = true
         swallow_regex = ^(foot)$
       }
@@ -111,8 +126,54 @@
       binde=,XF86AudioLowerVolume, exec, wpctl set-volume -l 1.4 @DEFAULT_AUDIO_SINK@ 5%-
       bind=,XF86AudioMute, exec, wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle
 
-      layerrule = blur, notifications
-      layerrule = ignorezero, notifications
+      gestures {
+          workspace_swipe = true
+          workspace_swipe_distance = 700
+          workspace_swipe_fingers = 3
+          workspace_swipe_cancel_ratio = 0.2
+          workspace_swipe_min_speed_to_force = 5
+          workspace_swipe_direction_lock = true
+          workspace_swipe_direction_lock_threshold = 10
+          workspace_swipe_create_new = true
+      }
+
+      # Window Rules
+      windowrulev2 = float,title:OpenSSH Authentication Passphrase request
+
+      ## Browser Picture in Picture
+      windowrulev2 = float, title:^([P|p]icture.*in.*[P|p]icture)$
+      windowrulev2 = pin, title:^([P|p]icture.*in.*[P|p]icture)$
+      windowrulev2 = move 69.5% 4%, title:^([P|p]icture.*in.*[P|p]icture)$
+
+      ## File dialogs
+      windowrulev2 = float, title:^((Save|Open) Files*)$
+      windowrulev2 = pin, title:^((Save|Open) Files*)$
+      windowrulev2 = size 75% 50%, title:^((Save|Open) Files*)$
+
+      windowrulev2 = opacity 0.95,class:^(chromium-browser)$
+      windowrulev2 = opacity 0.80,class:^(foot)$
+      windowrulev2 = opacity 0.80,class:^(qt5ct)$
+      windowrulev2 = opacity 0.80,class:^(qt6ct)$
+      windowrulev2 = opacity 0.80,class:^(org.pulseaudio.pavucontrol)$
+      windowrulev2 = opacity 0.80,class:blueman-manager
+      windowrulev2 = opacity 0.80,class:^(org.freedesktop.impl.portal.desktop.gtk)$
+      windowrulev2 = opacity 0.80,class:^(org.freedesktop.impl.portal.desktop.hyprland)$
+      windowrulev2 = opacity 0.70,class:^([Ss]team)$
+      windowrulev2 = opacity 0.70,class:^(steamwebhelper)$
+      windowrulev2 = opacity 0.70,class:^(Spotify)$
+      windowrulev2 = opacity 0.70,initialTitle:^(Spotify Free)$
+      windowrulev2 = opacity 0.70,initialTitle:^(Spotify Premium)$
+
+      windowrulev2 = float,class:^(foot)$,title:^(btop)$
+      windowrulev2 = float,class:^(foot)$,title:^(htop)$
+      windowrulev2 = float,class:^(qt5ct)$
+      windowrulev2 = float,class:^(qt6ct)$
+      windowrulev2 = float,class:^(org.pulseaudio.pavucontrol)$
+      windowrulev2 = float,class:blueman-manager
+      windowrulev2 = float,class:^(Signal)$ # Signal-Gtk
+
+      layerrule = blur,notifications
+      layerrule = ignorezero,notifications
 
       exec-once=${lib.getExe pkgs.swaybg} -m fill -i ${osConfig.desktopAssets.wallpaper}
     '';
