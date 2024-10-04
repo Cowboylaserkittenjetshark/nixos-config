165e0047cb5a26a868d2f9feebbabfaa13af0933
Cowboylaserkittenjetshark <82691052+Cowboylaserkittenjetshark@users.noreply.github.com>
2024-10-04 00:37:45 -0400

    feat: don't override hyprland's nixpkgs input

--- flake.nix
+++ flake.nix
@@ -16,10 +16,7 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    hyprland = {
-      url = "git+https://github.com/hyprwm/Hyprland?submodules=1";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    hyprland.url = "git+https://github.com/hyprwm/Hyprland?submodules=1";
 
     eww = {
       url = "github:elkowar/eww";
