dfa5d378091da7ffee29e0d74bbdf6f9f4f9330b
Cowboylaserkittenjetshark <82691052+Cowboylaserkittenjetshark@users.noreply.github.com>
2024-11-17 22:12:51 -0500

    chore: format

--- modules/programs/davinci-resolve.nix
+++ modules/programs/davinci-resolve.nix
@@ -6,7 +6,6 @@
 }: {
   config = lib.mkIf config.systemAttributes.graphical {
     hardware.amdgpu.opencl.enable = true;
-    environment.systemPackages = [ pkgs.davinci-resolve ];
-    
+    environment.systemPackages = [pkgs.davinci-resolve];
   };
 }
