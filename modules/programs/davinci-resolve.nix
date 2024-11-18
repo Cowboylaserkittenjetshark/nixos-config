7c2102b8a1b27fb6943830d574e3f8cf507805bd
Cowboylaserkittenjetshark <82691052+Cowboylaserkittenjetshark@users.noreply.github.com>
2024-11-17 22:12:27 -0500

    feat: add Davinci Resolve

---
+++ modules/programs/davinci-resolve.nix
@@ -0,0 +1,12 @@
+{
+  lib,
+  pkgs,
+  config,
+  ...
+}: {
+  config = lib.mkIf config.systemAttributes.graphical {
+    hardware.amdgpu.opencl.enable = true;
+    environment.systemPackages = [ pkgs.davinci-resolve ];
+    
+  };
+}
