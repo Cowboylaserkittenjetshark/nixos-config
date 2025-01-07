dbacab942bee6fb2b00563073ce11ae568a60301
Cowboylaserkittenjetshark <82691052+Cowboylaserkittenjetshark@users.noreply.github.com>
2025-01-07 15:46:27 -0500

    feat(helix): configure typst support

--- users/cblkjs/homeManagerModules/programs/helix.nix
+++ users/cblkjs/homeManagerModules/programs/helix.nix
@@ -1,6 +1,23 @@
-{pkgs, ...}: {
+{...}: {
   programs.helix = {
     enable = true;
     defaultEditor = true;
+    settings = {
+      # Needs https://github.com/helix-editor/helix/pull/11164
+      keys.normal."A-t" = ":run-shell-command sioyek %{filename}";
+    };
+    languages = {
+      language = [
+        {
+          name = "typst";
+          auto-format = true;
+        }
+      ];
+      language-server.tinymist.config = {
+        exportPdf = "onType";
+        outputPath = "$root/target/$dir/$name";
+        formatterMode = "typstyle";
+      };
+    };
   };
 }
