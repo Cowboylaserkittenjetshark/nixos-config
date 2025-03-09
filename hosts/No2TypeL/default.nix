5fd9cbb3e07ab2731ef54c5a23e2a58895ef6a38
Cowboylaserkittenjetshark <82691052+Cowboylaserkittenjetshark@users.noreply.github.com>
2025-03-09 18:49:20 -0400

    feat: temp enable octoprint

--- hosts/No2TypeL/default.nix
+++ hosts/No2TypeL/default.nix
@@ -84,5 +84,17 @@
 
   gaming.enable = true;
 
+  services.octoprint = {
+    enable = true;
+    host = "127.0.0.1";
+    plugins = plugins:
+      with plugins; [
+        printtimegenius
+        simpleemergencystop
+        themeify
+        titlestatus
+      ];
+  };
+
   system.stateVersion = "23.11";
 }
