539eb945e3787ea29a7f5331c6f448ae67734f34
Cowboylaserkittenjetshark <82691052+Cowboylaserkittenjetshark@users.noreply.github.com>
2025-07-22 20:29:50 -0400

    feat: add default roles for ski

--- homeManagerModules/programs/ski.nix
+++ homeManagerModules/programs/ski.nix
@@ -3,15 +3,25 @@
   imports = [
     inputs.ski.homeModules.default
   ];
+
   home.packages = [ inputs.ski.packages.${pkgs.system}.default ];
 
   programs.ski = {
     enable = true;
     settings = {
       pairs = {
-        yk.name = "yk_9767";
-        t2a.name = "t2_5610_auth";
-        t2s.name = "t2_5610_git";
+        yk = {
+          name = "yk_9767";
+          default_roles = [ "auth" "sign" ];
+        };
+        t2a = {
+          name = "t2_5610_auth";
+          default_roles = [ "auth" ];
+        };
+        t2s = {
+          name = "t2_5610_git";
+          default_roles = [ "sign" ];
+        };
       };
       roles = {
         auth.target = "id_ed25519_sk";
