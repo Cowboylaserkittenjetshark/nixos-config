dc503fcc0bfb102e823cbff37c479529debc3a77
Cowboylaserkittenjetshark <82691052+Cowboylaserkittenjetshark@users.noreply.github.com>
2026-06-03 16:05:14 -0400

    Remove unused auth refrence to hyprlock

--- nixosModules/authentication/default.nix
+++ nixosModules/authentication/default.nix
@@ -1,16 +1,6 @@
-{
-  lib,
-  config,
-  ...
-}:
-{
+_: {
   imports = [
     ./fprint.nix
     ./pam_u2f.nix
   ];
-
-  config = lib.mkIf config.systemAttributes.graphical {
-    # Allow wayland lockers to unlock the screen
-    security.pam.services.hyprlock.text = "auth include login";
-  };
 }
