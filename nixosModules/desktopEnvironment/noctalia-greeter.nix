e592c1313d33687c08dcc6b42191db34c03b933d
Cowboylaserkittenjetshark <82691052+Cowboylaserkittenjetshark@users.noreply.github.com>
2026-06-15 17:30:49 -0400

    set greeter user and session

--- nixosModules/desktopEnvironment/noctalia-greeter.nix
+++ nixosModules/desktopEnvironment/noctalia-greeter.nix
@@ -7,6 +7,7 @@
     enable = config.desktopEnvironment.enable;
     package = inputs.noctalia-greeter.packages.${pkgs.stdenv.hostPlatform.system}.default;
 
+    greeter-args = "--session Niri --user cblkjs";
     settings.cursor = let cfg = config.stylix.cursor; in {
       theme = cfg.name;
       inherit (cfg) size package;
