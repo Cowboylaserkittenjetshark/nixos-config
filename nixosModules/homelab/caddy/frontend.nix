e3d7edae3e71f31d95f92fe36f6537a871f89115
Cowboylaserkittenjetshark <82691052+Cowboylaserkittenjetshark@users.noreply.github.com>
2026-06-26 07:51:01 +0000

    fix frontend caddy

--- nixosModules/homelab/caddy/frontend.nix
+++ nixosModules/homelab/caddy/frontend.nix
@@ -2,6 +2,6 @@
   inherit (config.homelab) frontend backend domain;
 in {
   config = lib.mkIf frontend.enable {
-    services.caddy.virtualHosts."*.${domain}".extraConfig = "reverse_proxy ${backend.hostname}";
+    services.caddy.virtualHosts."${domain}, *.${domain}".extraConfig = "reverse_proxy ${backend.hostname}";
   };
 }
